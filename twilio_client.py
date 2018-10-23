2201e48d897a2183c102ebc75a95c90f59d586ff
jaysjin <jays.jin@yahoo.com>
2018-10-23 15:52:42 +0800

    fix some bug

--- twilio_client.py
+++ twilio_client.py
@@ -5,12 +5,10 @@ import time
 account_sid = '××××××××××××××××××××××××××××××××××××××'
 auth_token = '××××××××××××××××××××××××××××××'
 
-def send_sms(to, text, tw_mobile = '+××××××××××××××'):
+def send_sms(to, text, tw_mobile = '+×××××××××××××'):
     client = Client(account_sid, auth_token)
     try:
-        message = client.messages.create(body=text,
-                                         from = tw_mobile,
-                                         to=to)
+        message = client.messages.create(to=to, from_=tw_mobile, body=text)
         print('message status:', message.status)
         return True
     except Exception as e:
@@ -18,7 +16,7 @@ def send_sms(to, text, tw_mobile = '+××××××××××××××'):
         return False
 
 if __name__ == '__main__':
-    greetings = {'8':'早上好，该起床了', '12':'中午好，该吃午饭了', '15':'下午好，可以吃个水果', '20':'晚上好，该睡觉啦'}
+    greetings = {'7':'早上好，该起床了 From Jin', '12':'中午好，该吃午饭了', '15':'下午好, Summer, 可以吃个水果, From Jin', '20':'晚上好，该睡觉啦'}
 
     print('script running!')
     while True:
@@ -27,7 +25,7 @@ if __name__ == '__main__':
         for key in greetings.keys():
             if(now.hour == int(key)):
                 message = greetings.get(key, 'This is a test message')
-                res = send_sms(to='+××××××××××××××', text = message)
+                res = send_sms(to='+×××××××××××××', text = message)
                 if(res):
                     print('Message send ok at:', datetime.strftime(now, '%Y-%m-%d %H:%M:%S'))
                     time.sleep(60*60)
